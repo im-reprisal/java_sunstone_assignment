3a618878ac31fd2d29d04a53e03964099f5dc867
Nishant Rishab <imreprisal@gmail.com>
2022-01-14 23:21:01 +0530

    java assignment done

---
+++ src/TaxCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TaxCalculator{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int salary = sc.nextInt();
+        System.out.println("java calculates "+salary);
+        int tax_amount = 0;
+        if (salary <= 50000){
+            tax_amount += 0;
+        }
+        if (salary > 50000 && salary <= 60000){
+            tax_amount = (salary - 50000) * 10/100;
+        }
+        if (salary > 60000 && salary <= 150000){
+            tax_amount = (salary - 50000) * 20/100;
+        }
+        if (salary > 150000){
+            tax_amount = salary * 30/100;
+        }
+        System.out.println("income tax is "+tax_amount);
+    }
+}
